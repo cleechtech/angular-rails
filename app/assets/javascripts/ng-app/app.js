78f2f8a29ac9f5cfe2e5186dd788830f261c069e
Connor Leech <connorleech@gmail.com>
2014-08-28 11:31:57 -0700

    add dashboard and animations

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -11,7 +11,26 @@ angular.module('angular-rails', [
 			url: '/',
 			templateUrl: 'home.html',
 			controller: 'HomeCtrl'
-		});
+		})
+		// abstract parent state
+		.state('dashboard', {
+			abstract: true,
+			url: '/dashboard',
+			templateUrl: 'dashboard/layout.html'
+		})
+		// default when someone hits the dashboard
+		.state('dashboard.one', {
+			url: '',
+			templateUrl: 'dashboard/one.html'
+		})
+		.state('dashboard.two', {
+			url: '/two',
+			templateUrl: 'dashboard/two.html'
+		})
+		.state('dashboard.three', {
+			url: '/three',
+			templateUrl: 'dashboard/three.html'
+		})
 
 	$urlRouterProvider.otherwise('/');
 	$locationProvider.html5Mode(true);
